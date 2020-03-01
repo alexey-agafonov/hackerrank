b9611c48752e7542551ac11bf36472bb413c3a27
alexey-agafonov <aleks.xploid@gmail.com>
2020-03-01 14:45:13 +0300

    Removing and simplifying conditionals

--- day 3/src/dev/agafonov/ConditionalStatements.java
+++ day 3/src/dev/agafonov/ConditionalStatements.java
@@ -12,11 +12,11 @@ public class ConditionalStatements {
             if (n >= 1 && n <= 100) {
                 if (n % 2 != 0) {
                     System.out.println("Weird");
-                } else if (n >= 2 && n <= 5) {
+                } else if (n <= 5) {
                     System.out.println("Not Weird");
-                } else if (n >= 6 && n <= 20) {
+                } else if (n <= 20) {
                     System.out.println("Weird");
-                } else if (n >= 20) {
+                } else {
                     System.out.println("Not Weird");
                 }
             }
