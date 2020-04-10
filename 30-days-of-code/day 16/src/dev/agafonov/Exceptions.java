b0004d496d489cc60f5da8e7275fed31bf98f746
alexey-agafonov <aleks.xploid@gmail.com>
2020-04-11 02:43:19 +0300

    Add a solution to the 16th day of the challenge.

--- 30-days-of-code/day 16/src/dev/agafonov/Exceptions.java
+++ 30-days-of-code/day 16/src/dev/agafonov/Exceptions.java
@@ -1,1 +1,18 @@
+package dev.agafonov;
 
+import java.util.Scanner;
+
+class Exceptions {
+
+    public static void main(String[] args) {
+        try (var scanner = new Scanner(System.in)) {
+            var value = scanner.next();
+
+            try {
+                System.out.println(Integer.parseInt(value));
+            } catch (NumberFormatException ex) {
+                System.out.println("Bad String");
+            }
+        }
+    }
+}
